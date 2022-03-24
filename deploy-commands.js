235f9edc1ae1d7eb3d7aedb6a105201a3a43071b
Kole L <kolerbear123@gmail.com>
2022-03-23 23:07:45 -0700

    create discord bot that allows user to find floorprice

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder()
+    .setName('floorprice')
+    .setDescription('gets floor price')
+    .addStringOption(option => option.setName('collection').setDescription('MagicEden Collection Name'))
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
---
+++ index.js
@@ -0,0 +1,36 @@
+const {Client, Intents, ClientPresence, ReactionUserManager} = require('discord.js');
+const {token} = require('./config.json');
+const fetch = require('node-fetch');
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+client.once('ready', () => {
+    console.log('Ready!');
+})
+
+client.on('interactionCreate', async interaction => {
+
+    if (!interaction.isCommand()) return;
+
+    const {commandName} = interaction;
+
+    if (commandName === "floorprice"){
+        await interaction.deferReply();
+        const collection = interaction.options.getString('collection');
+        //const query = new URLSearchParams({collection});
+        //console.log(query)
+        const data = await fetch(`https://api-mainnet.magiceden.dev/v2/collections/${collection}/stats`)
+            .then(response => response.json());
+        
+        console.log(data.floorPrice);
+        
+        const floorPriceInSol = data.floorPrice/1000000000;
+        if(!data){
+            interaction.editReply(`No results found for **${collection}**`)
+        }else{
+            interaction.editReply(`**${collection}:** ${floorPriceInSol} SOL`)
+        }
+    }
+})
+
+client.login(token);
